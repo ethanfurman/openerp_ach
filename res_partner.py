fa74ca4fe5f6d503cec91edc89325d719bc923b7
Ethan Furman <ethan@stoneleaf.us>
2014-02-03 10:17:40 -0800

    ready to interact with external script

--- res_partner.py
+++ res_partner.py
@@ -8,41 +8,39 @@ class res_partner(osv.Model):
         'ach_type': fields.selection([('domestic', 'Domestic'), ('foreign', 'Foreign')], "Type of account"),
         'ach_routing': fields.char('ACH Routing #', size=9, help="Partner's bank's routing number"),
         'ach_account': fields.char('ACH Account #', size=32, help="Partner's account at bank."),
-        'ach_verified': fields.selection([('Not Verified','unverified'), ('Testing','testing'), ('Verified','verified')], 'ACH Status'),
+        'ach_verified': fields.selection([('unverified','Not Verified'), ('testing','Testing'), ('verified','Verified')], 'ACH Status'),
         'ach_amount': fields.integer('Ach Amount'),
+        'ach_date': fields.date('Last Transaction'),
         }
-
-    def ach_validation_check(self, cr, uid, *args):
-        to_test = self.browse(cr, uid, self.search(cr, uid, [('ach_verified','=','unverified')]))
-        passed = self.browse(cr, uid, self.search(cr, uid, [('ach_verified','=','verified'),('ach_amount','!=',0)]))
-        if to_test or passed:
-            ach_file = self._get_next_ach_file()
 
     def _validate_ach(self, values, record=None):
         if record is None:
-            existing = {}
-        else:
-            existing = dict(ach_type=record.ach_type, ach_routing=record.ach_routing, ach_account=record.ach_account)
+            record = PropertyDict(ach_type=False, ach_routing=False, ach_account=False)
         proposed = {}
-        proposed['ach_type'] = values.get('ach_type')
-        proposed['ach_routing'] = values.get('ach_routing')
-        proposed['ach_account'] = values.get('ach_account')
-        proposed.update(existing)
+        proposed['ach_type'] = values.get('ach_type', record.ach_type)
+        proposed['ach_routing'] = values.get('ach_routing', record.ach_routing)
+        proposed['ach_account'] = values.get('ach_account', record.ach_account)
         # either all three have values, or all three are empty
-        values = proposed.values()
-        if not (all_equal(values, lambda v: bool(v) is False) or all_equal(values, lambda v: bool(v) is True)):
+        proposed_values = proposed.values()
+        if all_equal(proposed_values, lambda v: bool(v) is False):
+            action = 'clear'
+        elif all_equal(proposed_values, lambda v: bool(v) is True):
+            action = 'set'
+        else:
             raise osv.except_osv('Information Missing', 'Either all the ACH fields must be filled out, or none of them')
-        route = proposed.get('ach_routing')
-        if route:
-            ACHPayment.validate_routing(route)
-        if proposed.get('ach_type') == 'foreign':
-            raise osv.except_osv('Not Implemented', 'Foreign ACH is not yet implemented.')
-        if 'ach_routing' in values or 'ach_account' in values:
-            # if the fields are being blanked out, blank out the dependencies as well
-            if not values['ach_routing']:
-                values['ach_verified'] = False
-                values['ach_amount'] = False
-            else:
+        if action == 'clear':
+            values['ach_verified'] = False
+            values['ach_amount'] = False
+            values['ach_date'] = False
+        elif action == 'set':
+            if proposed.get('ach_type') == 'foreign':
+                raise osv.except_osv('Not Implemented', 'Foreign ACH is not yet implemented.')
+            route = proposed['ach_routing']
+            try:
+                ACHPayment.validate_routing(route)
+            except ValueError:
+                raise osv.except_osv('Invalid', 'Routing number %s fails check digit calculation' % route)
+            if 'ach_account' in values or 'ach_routing' in values:
                 values['ach_verified'] = 'unverified'
                 values['ach_amount'] = 0
         return True
@@ -52,16 +50,12 @@ class res_partner(osv.Model):
         return super(res_partner, self).create(cr, uid, values, context=context)
 
     def write(self, cr, uid, ids, values, context=None):
-        if len(ids) <= 2:
-            if ids:
-                record = self.browse(cr, uid, ids[0], context=context)
-            else:
-                record = None
-            self._validate_ach(values, record)
-        else:
-            for id, new_values in values.items():
+        if ids:
+            if isinstance(ids, (int, long)):
+                ids = [ids]
+            for id in ids:
                 record = self.browse(cr, uid, id, context=context)
-                self._validate_ach(new_values, record)
+                self._validate_ach(values, record)
         return super(res_partner, self).write(cr, uid, ids, values, context=context)
 
 res_partner()
