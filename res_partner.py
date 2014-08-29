2d15aaec64ee179cc3367c825dad9b82bd89d4d2
Ethan Furman <ethan@stoneleaf.us>
2014-08-29 10:51:12 -0700

    refactor validate_ach

--- res_partner.py
+++ res_partner.py
@@ -1,5 +1,4 @@
-from fnx import all_equal, PropertyDict
-from fnx.finance import ACHPayment
+from fnx import validate_ach
 from openerp.osv import fields, osv
 
 class res_partner(osv.Model):
@@ -13,49 +12,16 @@ class res_partner(osv.Model):
         'ach_date': fields.date('Last Transaction'),
         }
 
-    def _validate_ach(self, values, record=None):
-        if record is None:
-            record = PropertyDict(ach_type=False, ach_routing=False, ach_account=False)
-        proposed = {}
-        proposed['ach_type'] = values.get('ach_type', record.ach_type)
-        proposed['ach_routing'] = values.get('ach_routing', record.ach_routing)
-        proposed['ach_account'] = values.get('ach_account', record.ach_account)
-        # either all three have values, or all three are empty
-        proposed_values = proposed.values()
-        if all_equal(proposed_values, lambda v: bool(v) is False):
-            action = 'clear'
-        elif all_equal(proposed_values, lambda v: bool(v) is True):
-            action = 'set'
-        else:
-            raise osv.except_osv('Information Missing', 'Either all the ACH fields must be filled out, or none of them')
-        if action == 'clear':
-            values['ach_verified'] = False
-            values['ach_amount'] = False
-            values['ach_date'] = False
-        elif action == 'set':
-            if proposed.get('ach_type') == 'foreign':
-                raise osv.except_osv('Not Implemented', 'Foreign ACH is not yet implemented.')
-            route = proposed['ach_routing']
-            try:
-                ACHPayment.validate_routing(route)
-            except ValueError:
-                raise osv.except_osv('Invalid', 'Routing number %s fails check digit calculation' % route)
-            if 'ach_account' in values or 'ach_routing' in values:
-                values['ach_verified'] = 'unverified'
-                values['ach_amount'] = 0
-        return True
-
     def create(self, cr, uid, values, context=None):
-        self._validate_ach(values)
+        validate_ach(values)
         return super(res_partner, self).create(cr, uid, values, context=context)
 
     def write(self, cr, uid, ids, values, context=None):
         if ids:
             if isinstance(ids, (int, long)):
                 ids = [ids]
-            for id in ids:
-                record = self.browse(cr, uid, id, context=context)
-                self._validate_ach(values, record)
+            for partner in self.browse(cr, uid, id, context=context):
+                validate_ach(values, partner)
         return super(res_partner, self).write(cr, uid, ids, values, context=context)
 
 res_partner()
